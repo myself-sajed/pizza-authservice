8631f8e6d06d998dc6b5660146939106038da7e4
myself-sajed <shaikhsajedcoolpop@gmail.com>
2023-09-29 20:17:13 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,28 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import createHttpError, { HttpError } from "http-errors";
 
 const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.get("/", (req, res) => {
+    const err = createHttpError(401, "Something went wrong");
+    throw err;
+});
+
+// Global error handling
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
+});
 
 export default app;
