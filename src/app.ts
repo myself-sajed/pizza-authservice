5b0d0b718a14508dc821b208e2f4c919de9ca7ce
myself-sajed <shaikhsajedcoolpop@gmail.com>
2023-10-01 12:41:45 +0530

    containerize the application

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response, NextFunction } from "express";
-import createHttpError, { HttpError } from "http-errors";
+import { HttpError } from "http-errors";
 
 const app = express();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.get("/", (req, res) => {
-    const err = createHttpError(401, "Something went wrong");
-    throw err;
-    // res.send({ status: "success" });
+    // const err = createHttpError(401, "Something went wrong");
+    // throw err;
+    res.send("Welcome to Pizza Auth Service");
 });
 
 // Global error handling
