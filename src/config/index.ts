27a2d4e2bed360203b97e217b646fb2a9b180d48
myself-sajed <shaikhsajedcoolpop@gmail.com>
2023-10-02 21:29:00 +0530

    add typeORM

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,17 @@
 import { config } from "dotenv";
+import path from "path";
 
-config();
+config({ path: path.join(__dirname, `../../env.${process.env.NODE_ENV}`) });
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_HOST, DB_PASS, DB_PORT, DB_NAME, DB_USERNAME } =
+    process.env;
 
 export const Config = {
     PORT,
     NODE_ENV,
+    DB_HOST,
+    DB_PASS,
+    DB_PORT,
+    DB_NAME,
+    DB_USERNAME,
 };
