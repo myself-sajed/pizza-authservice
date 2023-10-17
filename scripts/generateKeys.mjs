863fa6d1c7fe6ba42bb6492dd2a77303092fd48f
myself-sajed <shaikhsajedcoolpop@gmail.com>
2023-10-17 22:06:16 +0530

    public and private keys generator

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,22 +1,22 @@
-import crypto from 'crypto';
-import fs from 'fs';
+import crypto from "crypto";
+import fs from "fs";
 
 const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
     modulusLength: 2048,
     privateKeyEncoding: {
-        type: 'pkcs1',
-        format: 'pem'
+        type: "pkcs1",
+        format: "pem",
     },
     publicKeyEncoding: {
-        type: 'pkcs1',
-        format: 'pem'
-    }
-})
+        type: "pkcs1",
+        format: "pem",
+    },
+});
 
-console.log('Public key:', publicKey)
-console.log('Private key:', privateKey)
+console.log("Public key:", publicKey);
+console.log("Private key:", privateKey);
 
 // writing the keys into .pem files
 
-fs.writeFileSync('certs/privateKey.pem', privateKey)
-fs.writeFileSync('certs/publicKey.pem', publicKey)
+fs.writeFileSync("certs/privateKey.pem", privateKey);
+fs.writeFileSync("certs/publicKey.pem", publicKey);
