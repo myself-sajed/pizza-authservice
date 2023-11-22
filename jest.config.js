d08f78ded2d4f564f41c1f8d67d64cff445e506a
myself-sajed <shaikhsajedcoolpop@gmail.com>
2023-11-22 18:13:47 +0530

    code coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,11 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
+    collectCoverage: true,
+    coverageProvider: 'v8',
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!test/**",
+        "!**/node_module/**"
+    ]
 };
