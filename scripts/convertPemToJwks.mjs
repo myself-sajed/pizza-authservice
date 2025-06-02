167681a20264f6d81a0d38210b2fda62cf947bf0
sajed-evsp <sajed@almochat.com>
2025-06-02 22:48:38 +0530

    create an mjs file to generate jwk set

--- scripts/convertPemToJwks.mjs
+++ scripts/convertPemToJwks.mjs
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import fs from "fs";
 import rsaPemToJwk from "rsa-pem-to-jwk";
 
 const privateKey = fs.readFileSync("./certs/privateKey.pem");
 const jwk = rsaPemToJwk(privateKey, { use: "sig" }, "public");
+
+console.log(JSON.stringify(jwk))
